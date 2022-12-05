142842de7477f64634c644cde76da17874766e62
lsm <487005831@qq.com>
2022-12-06 00:35:06 +0800

    vue

---
+++ vue-ts-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    hello app
+
+    <el-button type='primary'>primary</el-button>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/reset.scss";
+@import "@/assets/styles/iconfont.scss";
+@import "@/assets/styles/common.scss";
+</style>
