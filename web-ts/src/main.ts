58105abbc634f2cbe5b386f2c1c110e4aeccfd32
lsm <487005831@qq.com>
2022-12-11 01:25:52 +0800

    store, key

--- web-ts/src/main.ts
+++ web-ts/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store, { key } from './store'
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
@@ -14,7 +14,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(store)
+app.use(store, key)
 app.use(router)
 app.use(ElementPlus, {
   locale: zhCn
